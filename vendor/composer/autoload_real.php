7133805f32b846ab74c039ada7328dfa1165462c
Francis Whittle <francis@agileware.com.au>
2022-03-16 14:42:43 +1100

    CIVIEWAY-227 Upgrade eWAY PHP SDK

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,19 +13,24 @@ class ComposerAutoloaderInite3e0b2b852364e5aafd9a67d9b0c9c63
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInite3e0b2b852364e5aafd9a67d9b0c9c63', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInite3e0b2b852364e5aafd9a67d9b0c9c63', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInite3e0b2b852364e5aafd9a67d9b0c9c63::getInitializer($loader));
         } else {
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '25c617a91609032d0fefc9014419c02f128cf05d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '25c617a91609032d0fefc9014419c02f128cf05d',
+            'dev_requirement' => false,
+        ),
+        'eway/eway-rapid-php' => array(
+            'pretty_version' => '1.4.0',
+            'version' => '1.4.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../eway/eway-rapid-php',
+            'aliases' => array(),
+            'reference' => '5581c8279774aabe58ea3aca9bfd7f62759ce7cd',
+            'dev_requirement' => false,
+        ),
+    ),
+);
