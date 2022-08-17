4b66a9a8850e7d5d34d2ed95f8c6b55431315274
Francis Whittle <francis@agileware.com.au>
2022-08-17 13:56:27 +1000

    CIVIEWAY-235 eway-rapid-php 1.4.1

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,30 +25,11 @@ class ComposerAutoloaderInite3e0b2b852364e5aafd9a67d9b0c9c63
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInite3e0b2b852364e5aafd9a67d9b0c9c63', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInite3e0b2b852364e5aafd9a67d9b0c9c63', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInite3e0b2b852364e5aafd9a67d9b0c9c63::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInite3e0b2b852364e5aafd9a67d9b0c9c63::getInitializer($loader));
 
         $loader->register(true);
 
