1824e5d4e74394f642950b32a14491c8ae6c6de3
Justin Freeman <justin@agileware.com.au>
2023-01-23 10:50:32 +1100

    CIVIEWAY-242 Fatal error: Uncaught Error: Call to undefined function _ewayrecurring_civix_civicrm_caseTypes() and also removed unused function, ewayrecurring_civicrm_angularModules (hook_civicrm_angularModules)

--- eWAYRecurring.php
+++ eWAYRecurring.php
@@ -256,33 +256,6 @@ function ewayrecurring_civicrm_managed(&$entities) {
 }
 
 /**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function ewayrecurring_civicrm_caseTypes(&$caseTypes) {
-  _ewayrecurring_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function ewayrecurring_civicrm_angularModules(&$angularModules) {
-  _ewayrecurring_civix_civicrm_angularModules($angularModules);
-}
-
-/**
  * Implements hook_civicrm_alterSettingsFolders().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
