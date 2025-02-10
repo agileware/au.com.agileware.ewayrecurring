d5285fbe6e48e2e6545e822dc0554af0e7c90fd0
Francis Whittle <francis@agileware.com.au>
2025-02-10 11:43:53 +1100

    CIVIEWAY-279 Update eWAY Rapid PHP library to version 1.4.2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'c512aa91ec9d050782df1d6fc73adcb3d335ecbd',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'c512aa91ec9d050782df1d6fc73adcb3d335ecbd',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'eway/eway-rapid-php' => array(
-            'pretty_version' => '1.4.1',
-            'version' => '1.4.1.0',
-            'reference' => '3ccc17406101db7e6670b2e76b088124c6955c14',
+            'pretty_version' => '1.4.2',
+            'version' => '1.4.2.0',
+            'reference' => '9d0b74fb0710f9f1ffeb015d198fc85b083ab5ba',
             'type' => 'library',
             'install_path' => __DIR__ . '/../eway/eway-rapid-php',
             'aliases' => array(),
